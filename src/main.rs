730754863bdcc65a0d89d3c730cd69a373ef269f
James Tingen <jotingen@gmail.com>
2022-01-18 19:27:31 -0500

    Draw a sphere

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,18 @@ fn write_color(pixel_color: Color) {
     );
 }
 
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+    let oc: Vec3 = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
 fn ray_color(r: Ray) -> Color {
+    if hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, r) {
+        return Color::new(1.0,0.0,0.0)
+    }
     let unit_direction: Vec3 = r.direction().unit_vector();
     let t: f64 = 0.5 * (unit_direction.y() + 1.0);
     Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
